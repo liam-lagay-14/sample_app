b69012cc46a348aeac84194da85efb440b61424e
Liam Lagay <liam.lagay@sage.com>
2014-09-15 10:27:18 +0100

    Adding Contact us pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'Static Pages' do
+
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe 'Home page' do
     it 'should have the content sample app' do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +13,7 @@ describe 'Static Pages' do
 
     it 'should have the title Home' do
       visit 'static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -22,7 +25,7 @@ describe 'Static Pages' do
 
     it 'should have the title Help' do
       visit 'static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -34,7 +37,20 @@ describe 'Static Pages' do
 
     it 'should have the title About Us' do
       visit 'static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe 'Contact Us Page' do
+    it 'should have the content Contact Us' do
+      visit 'static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it 'should have the title Contact Us' do
+      visit 'static_pages/contact'
+
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
